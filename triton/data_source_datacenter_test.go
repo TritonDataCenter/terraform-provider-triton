6b4a0eda4bbac546db7a63e8648b8561ec389e12
stack72 <public@paulstack.co.uk>
2018-01-04 20:22:44 +0200

    datasource/datacenter: Cater for old Joyent Cloud URLS in datasource
    ```
    terraform-provider-triton [master●] % acctests triton TestAccTritonDataCenter
    === RUN   TestAccTritonDataCenter
    --- PASS: TestAccTritonDataCenter (4.68s)
    === RUN   TestAccTritonDataCenterOldUrl
    --- PASS: TestAccTritonDataCenterOldUrl (4.66s)
    PASS
    ok  	github.com/terraform-providers/terraform-provider-triton/triton	9.362s
    ```

--- triton/data_source_datacenter_test.go
+++ triton/data_source_datacenter_test.go
@@ -23,10 +23,36 @@ func TestAccTritonDataCenter(t *testing.T) {
 	})
 }
 
+func TestAccTritonDataCenterOldUrl(t *testing.T) {
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccTritonDataCenterOldUrl,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.triton_datacenter.current", "name", "us-east-1"),
+					resource.TestCheckResourceAttr("data.triton_datacenter.current", "endpoint", "https://us-east-1.api.joyentcloud.com"),
+				),
+			},
+		},
+	})
+}
+
 var testAccTritonDataCenter = `
 
 provider "triton" {
   url = "https://us-sw-1.api.joyentcloud.com"
+}
+
+data "triton_datacenter" "current" {}
+`
+
+var testAccTritonDataCenterOldUrl = `
+
+provider "triton" {
+  url = "https://us-east-1.api.joyent.com"
 }
 
 data "triton_datacenter" "current" {}
