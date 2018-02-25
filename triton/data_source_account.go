1ee5076ccef1fd10b6beb59d0e0d0465066130c1
Krzysztof Wilczynski <kw@linux.com>
2018-02-25 19:46:57 +0100

    Update the triton_account data source.
    This commit updates the `triton_account` data source with the following changes:
    
      - Adds more logging at the debug level
      - Adds more comments to the code
      - Add new `login` and `email` attributes to the data source output
      - Updates error messages
      - Updates documentation
      - Removes an underscore from the variable name in the acceptance test
    
    Signed-off-by: Krzysztof Wilczynski <kw@linux.com>

--- triton/data_source_account.go
+++ triton/data_source_account.go
@@ -2,15 +2,26 @@ package triton
 
 import (
 	"context"
+	"log"
 
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/joyent/triton-go/account"
+	"github.com/pkg/errors"
 )
 
+// dataSourceAccount returns schema for the Account data source.
 func dataSourceAccount() *schema.Resource {
 	return &schema.Resource{
 		Read: dataSourceAccountRead,
 		Schema: map[string]*schema.Schema{
+			"login": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"email": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
 			"cns_enabled": {
 				Type:     schema.TypeBool,
 				Computed: true,
@@ -19,20 +30,28 @@ func dataSourceAccount() *schema.Resource {
 	}
 }
 
+// dataSourceAccountRead retrieves details about current Account from Triton
+// using the Account API. The current Account name will be the same as the
+// one currently configured in the Triton provider.
 func dataSourceAccountRead(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*Client)
+
 	c, err := client.Account()
 	if err != nil {
-		return err
+		return errors.Wrap(err, "error creating Account client")
 	}
 
+	log.Printf("[DEBUG] triton_account: Reading Account details.")
 	acc, err := c.Get(context.Background(), &account.GetInput{})
 	if err != nil {
-		return err
+		return errors.Wrap(err, "error retrieving Account details")
 	}
 
+	log.Printf("[DEBUG] triton_account: Found matching Account: %+v", acc)
 	d.SetId(acc.ID)
 
+	d.Set("login", acc.Login)
+	d.Set("email", acc.Email)
 	d.Set("cns_enabled", acc.TritonCNSEnabled)
 
 	return nil
