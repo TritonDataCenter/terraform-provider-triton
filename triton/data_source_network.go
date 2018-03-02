1471bcb331ee17c842712bf5f2fd5cf6ccb93d84
Krzysztof Wilczynski <kw@linux.com>
2018-03-02 02:41:21 +0100

    Update the triton_network data source.
    This commit updates the `triton_network` data source with the following changes:
    
      - Adds more logging at the debug level
      - Adds more comments to the code
      - Updates error messages
      - Updates documentation
      - Add new `public` and `fabric` attributes to the data source output
      - Removes an underscore from the variable name in the acceptance test
    
    Signed-off-by: Krzysztof Wilczynski <kw@linux.com>

--- triton/data_source_network.go
+++ triton/data_source_network.go
@@ -3,55 +3,70 @@ package triton
 import (
 	"context"
 	"fmt"
+	"log"
 
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/joyent/triton-go/network"
+	"github.com/pkg/errors"
 )
 
+// dataSourceNetwork returns schema for the Network data source.
 func dataSourceNetwork() *schema.Resource {
 	return &schema.Resource{
 		Read: dataSourceNetworkRead,
 		Schema: map[string]*schema.Schema{
 			"name": {
 				Type:     schema.TypeString,
-				Optional: true,
+				Required: true,
 				ForceNew: true,
+			},
+			"public": {
+				Type:     schema.TypeBool,
+				Computed: true,
+			},
+			"fabric": {
+				Type:     schema.TypeBool,
+				Computed: true,
 			},
 		},
 	}
 }
 
+// dataSourceNetworkRead retrieves details about all the networks which
+// can be used by the given account from the Networks API, then searches
+// for a matching network in the list of available networks using network
+// name as a filter.
 func dataSourceNetworkRead(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*Client)
+
 	net, err := client.Network()
 	if err != nil {
-		return err
+		return errors.Wrap(err, "error creating Network client")
 	}
 
+	log.Printf("[DEBUG] triton_network: Reading Network details.")
 	networks, err := net.List(context.Background(), &network.ListInput{})
 	if err != nil {
-		return err
-	}
-	if len(networks) == 0 {
-		return fmt.Errorf("Your query returned no results. Please change " +
-			"your search criteria and try again.")
+		return errors.Wrap(err, "error retrieving Network details")
 	}
 
-	var networkName string
-	if name, hasName := d.GetOk("name"); hasName {
-		networkName = name.(string)
-	}
+	networkName := d.Get("name").(string)
 
-	var network *network.Network
-	for _, found := range networks {
-		if found.Name == networkName {
-			network = found
+	var result *network.Network
+	for _, network := range networks {
+		if network.Name == networkName {
+			log.Printf("[DEBUG] triton_network: Found matching Network: %+v", network)
+			result = network
+			break
 		}
 	}
-	if network == nil {
-		return fmt.Errorf("No Networks found by name %q", networkName)
+	if result == nil {
+		return fmt.Errorf("no matching Network with name %q found", networkName)
 	}
 
-	d.SetId(network.Id)
+	d.SetId(result.Id)
+	d.Set("public", result.Public)
+	d.Set("fabric", result.Fabric)
+
 	return nil
 }
